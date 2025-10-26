3f840c5f7330a56478ceac40c26fc7e00f1dbfed
Marcus Melo <marcusmelostar@gmail.com>
2025-10-26 11:34:00 -0300

    15

--- scripts/node_4407255ae0f62.tmp(1).js
+++ scripts/node_4407255ae0f62.tmp(1).js
@@ -1,8 +1,10 @@
  const escolhas = document.querySelectorAll(".escolha")
-escolhas.forEach(tags=> {
+    escolhas.forEach(tags=> {
     tags.addEventListener("click", (e) => {
         e.preventDefault()  
-        alert(tags.textContent)
+        
+        console.log(tags)
+
     } )
  
 })
